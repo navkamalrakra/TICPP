4844dce9c33c0db81f8ec9aa0412ffbd068b04cc
Mojo Jojo <navkamal90@gmail.com>
2017-08-29 18:32:00 +0530

    Add files via upload

---
+++ C15E4.cpp
@@ -0,0 +1,36 @@
+//: C14:Combined.cpp
+// Inheritance & composition
+
+class A {
+	int i;
+public:
+	A(int ii) : i(ii) {}
+	~A() {}
+	virtual void f() const {}
+};
+
+class B {
+	int i;
+public:
+	B(int ii) : i(ii) {}
+	~B() {}
+	void f() const {}
+};
+
+class C : public B {
+	A a;
+public:
+	C(int ii) : B(ii), a(ii) {}
+	~C() {} // Calls ~A() and ~B()
+	void f() const {  // Redefinition
+		a.f();
+		B::f();
+	}
+};
+
+int main() {
+//	C c(47);
+	B *cp = new C(47);
+	cp->f();
+	return 0;
+} ///:~
