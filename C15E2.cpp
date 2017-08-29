caf279a215228c028d10d4aa3eb7c4e888f2c8a4
Mojo Jojo <navkamal90@gmail.com>
2017-08-29 17:59:06 +0530

    Add files via upload

---
+++ C15E2.cpp
@@ -0,0 +1,56 @@
+// C15E1.cpp : Defines the entry point for the console application.
+//
+
+#include <iostream>
+
+class shape {
+public:
+	virtual void draw() = 0;
+	shape()
+	{
+		draw();
+	}
+};
+
+void shape::draw() {
+	std::cout << "shape::draw()\n";
+}
+
+class circle :public shape
+{
+	void draw()
+	{
+		std::cout << "circle::draw()\n";
+	}
+
+};
+
+class square :public shape
+{
+	void draw()
+	{
+		std::cout << "square::draw()\n";
+	}
+
+};
+
+class triangle :public shape
+{
+	void draw()
+	{
+		std::cout << "trianlge::draw()\n";
+	}
+
+};
+
+
+int main()
+{
+	shape *p[] = { /*new shape,*/new circle,new square, new triangle };
+	p[0]->draw();
+	p[1]->draw();
+	p[2]->draw();
+	//p[3]->draw();
+	return 0;
+}
+
